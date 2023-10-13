e1b52001cdfa7c4caf708f39630c81e233bd58a2
Shaatvika <srishaatvika@gmail.com>
2023-10-13 20:58:43 +0530

    Solving issue #5

--- box.py
+++ box.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame 
 import random
 
 # Initialize Pygame
@@ -24,8 +24,8 @@ WIDTH_OF_LINE = 5
 inc_or_dec_up_down = -1
 inc_or_dec_left_right = 1
 inc_value = HEIGHT/100
-x_pos = 0.2*WIDTH+2*WIDTH_OF_LINE
-y_pos = 0.8 * HEIGHT
+x_pos = random.uniform(0.2*WIDTH, 0.7*WIDTH)
+y_pos = random.uniform(0.1 * HEIGHT, 0.8 * HEIGHT)
 counter = 10
 
 color_inc_or_dec = [1,1,1]
