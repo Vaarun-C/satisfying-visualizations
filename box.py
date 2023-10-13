714365e5d9cde7e9d3c96f4ff0244b8858c8a623
BizBrainiac <125855598+BizBrainiac@users.noreply.github.com>
2023-10-14 00:01:21 +0530

    finally done

--- box.py
+++ box.py
@@ -7,10 +7,11 @@ pygame.init()
 
 # Initialize the Pygame mixer
 pygame.mixer.init()
-mixer.music.load (r"gg.mp3")
+mixer.music.load("gg.mp3")
 mixer.music.play(-1)
 
-
+# Load the collision sound effect
+collision_sound = mixer.Sound("durm.mp3")
 
 WIDTH, HEIGHT = 900, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,43 +22,43 @@ run = True
 FPS = 60
 GREY = (128, 128, 128)
 DARKGREY = (65, 65, 65)
-GRADIENT_START = (30,30,30)
+GRADIENT_START = (30, 30, 30)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 COLOUR = GRADIENT_START
 WIDTH_OF_LINE = 5
 inc_or_dec_up_down = -1
 inc_or_dec_left_right = 1
-inc_value = HEIGHT/100
-x_pos = random.randint(100,840)
-y_pos = random.randint(100,730)
+inc_value = HEIGHT / 100
+x_pos = random.randint(100, 840)
+y_pos = random.randint(100, 730)
 counter = 10
-
-color_inc_or_dec = [1,1,1]
+color_inc_or_dec = [1, 1, 1]
 
 def new_colour(colour):
-
     global color_inc_or_dec
-
     colour = list(colour)
     index = random.randint(0, 2)
-    if(colour[index] >= 230) or (colour[index] <= 0):
+    if colour[index] >= 230 or colour[index] <= 0:
         color_inc_or_dec[index] *= -1
-    colour[index] = (colour[index] + counter*color_inc_or_dec[index])
+    colour[index] = (colour[index] + counter * color_inc_or_dec[index])
     return tuple(colour)
 
 def draw_window():
-
     global x_pos, y_pos, inc_or_dec_left_right, inc_or_dec_up_down, COLOUR
-
-    outer_box = pygame.Rect(0.2*WIDTH+WIDTH_OF_LINE, 0.1*HEIGHT+WIDTH_OF_LINE, 0.6*WIDTH, 0.8*HEIGHT)
-    inner_box = pygame.Rect(x_pos, y_pos, 0.1 * HEIGHT, 0.1*HEIGHT)
     COLOUR = new_colour(COLOUR)
-    # print(COLOUR)
+    WIN.fill(GREY)
 
+    outer_box = pygame.Rect(0.2 * WIDTH + WIDTH_OF_LINE, 0.1 * HEIGHT + WIDTH_OF_LINE, 0.6 * WIDTH, 0.8 * HEIGHT)
+    inner_box = pygame.Rect(x_pos, y_pos, 0.1 * HEIGHT, 0.1 * HEIGHT)
+
+    # Draw the outer box
     pygame.draw.rect(WIN, WHITE, outer_box, width=WIDTH_OF_LINE)
+    pygame.draw.rect(WIN, DARKGREY, outer_box, width=WIDTH_OF_LINE // 2)
+
+    # Draw the inner box
     pygame.draw.rect(WIN, COLOUR, inner_box)
-    pygame.draw.rect(WIN, DARKGREY, inner_box, width=WIDTH_OF_LINE//2)
+    pygame.draw.rect(WIN, DARKGREY, inner_box, width=WIDTH_OF_LINE // 2)
 
     if outer_box.colliderect(inner_box):
         if outer_box.top >= inner_box.top:
@@ -76,29 +77,24 @@ def draw_window():
             # collision_direction: right
             inc_or_dec_left_right *= -1
 
-
-collision_sound = mixer.Sound(r"durm.mp3")
-if outer_box.colliderect(inner_box):
-    collision_sound.play()
-
-
-    x_pos += inc_value*inc_or_dec_left_right
-    y_pos += inc_value*inc_or_dec_up_down
+    x_pos += inc_value * inc_or_dec_left_right
+    y_pos += inc_value * inc_or_dec_up_down
 
     pygame.display.update()
     pygame.time.delay(60)
 
-
 def main():
     global run
+
     WIN.fill(GREY)
 
-    # Main game loop    
+    # Main game loop
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 continue
+
         draw_window()
 
     pygame.quit()
